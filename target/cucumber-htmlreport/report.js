3277ea872a5ef3f21c2acc52fdb3f5d3370cbb75
Admin <Admin@LAPTOP-JVHRMR7J>
2019-05-05 10:13:00 +0530

    my second commit

--- target/cucumber-htmlreport/report.js
+++ target/cucumber-htmlreport/report.js
@@ -1,33 +1,451 @@
-$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("com/basic/requestSpecifications/GetRequestTesting.feature");
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("GetWithCucumberTags.feature");
 formatter.feature({
-  "line": 1,
-  "name": "First Rest Assured test",
+  "line": 2,
+  "name": "First Rest Assured test44",
   "description": "",
-  "id": "first-rest-assured-test",
-  "keyword": "Feature"
+  "id": "first-rest-assured-test44",
+  "keyword": "Feature",
+  "tags": [
+    {
+      "line": 1,
+      "name": "@Important"
+    }
+  ]
 });
-formatter.before({
-  "duration": 534186,
+formatter.scenario({
+  "line": 5,
+  "name": "Test my Get Method55",
+  "description": "",
+  "id": "first-rest-assured-test44;test-my-get-method55",
+  "type": "scenario",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "line": 4,
+      "name": "@Smoke"
+    }
+  ]
+});
+formatter.step({
+  "line": 6,
+  "name": "user start the rest asured test",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 7,
+  "name": "user hit the get request",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 8,
+  "name": "user checks 200 the status code",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 9,
+  "name": "user checks the count or size",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 10,
+  "name": "user checks \"albert_park\" value",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 11,
+  "name": "user checks at path \"MRData.CircuitTable.Circuits.circuitId[0]\" with the \"albert_park\" value",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "MyGetWithTagsSD.user_start_the_rest_asured_test()"
+});
+formatter.result({
+  "duration": 102232659,
+  "status": "passed"
+});
+formatter.match({
+  "location": "MyGetWithTagsSD.user_hit_the_get_request()"
+});
+formatter.result({
+  "duration": 3323217359,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "200",
+      "offset": 12
+    }
+  ],
+  "location": "MyGetWithTagsSD.user_checks_the_status_code(int)"
+});
+formatter.result({
+  "duration": 10041450,
+  "status": "passed"
+});
+formatter.match({
+  "location": "MyGetWithTagsSD.user_checks_the_count_or_size()"
+});
+formatter.result({
+  "duration": 628791215,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "albert_park",
+      "offset": 13
+    }
+  ],
+  "location": "MyGetWithTagsSD.user_checks_circuitId_value(String)"
+});
+formatter.result({
+  "duration": 56600982,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "MRData.CircuitTable.Circuits.circuitId[0]",
+      "offset": 21
+    },
+    {
+      "val": "albert_park",
+      "offset": 74
+    }
+  ],
+  "location": "MyGetWithTagsSD.user_checks_at_path_with_the_value(String,String)"
+});
+formatter.result({
+  "duration": 34909837,
   "status": "passed"
 });
 formatter.scenario({
-  "line": 4,
-  "name": "Test my Get Method",
+  "line": 15,
+  "name": "Test my Get Method56",
   "description": "",
-  "id": "first-rest-assured-test;test-my-get-method",
+  "id": "first-rest-assured-test44;test-my-get-method56",
   "type": "scenario",
-  "keyword": "Scenario"
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "line": 14,
+      "name": "@Smoke"
+    },
+    {
+      "line": 14,
+      "name": "@Regression"
+    }
+  ]
 });
 formatter.step({
-  "line": 5,
-  "name": "user hit the get request",
+  "line": 16,
+  "name": "user start the rest asured test",
   "keyword": "Given "
 });
+formatter.step({
+  "line": 17,
+  "name": "user hit the get request",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 18,
+  "name": "user checks 200 the status code",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 19,
+  "name": "user checks the count or size",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 20,
+  "name": "user checks \"albert_park\" value",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 21,
+  "name": "user checks at path \"MRData.CircuitTable.Circuits.circuitId[0]\" with the \"albert_park\" value",
+  "keyword": "Then "
+});
 formatter.match({
-  "location": "RequestSpecificationSD.user_hit_the_get_request()"
+  "location": "MyGetWithTagsSD.user_start_the_rest_asured_test()"
 });
 formatter.result({
-  "duration": 1163414332,
+  "duration": 122880,
+  "status": "passed"
+});
+formatter.match({
+  "location": "MyGetWithTagsSD.user_hit_the_get_request()"
+});
+formatter.result({
+  "duration": 9323999013,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "200",
+      "offset": 12
+    }
+  ],
+  "location": "MyGetWithTagsSD.user_checks_the_status_code(int)"
+});
+formatter.result({
+  "duration": 199680,
+  "status": "passed"
+});
+formatter.match({
+  "location": "MyGetWithTagsSD.user_checks_the_count_or_size()"
+});
+formatter.result({
+  "duration": 164560073,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "albert_park",
+      "offset": 13
+    }
+  ],
+  "location": "MyGetWithTagsSD.user_checks_circuitId_value(String)"
+});
+formatter.result({
+  "duration": 37011311,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "MRData.CircuitTable.Circuits.circuitId[0]",
+      "offset": 21
+    },
+    {
+      "val": "albert_park",
+      "offset": 74
+    }
+  ],
+  "location": "MyGetWithTagsSD.user_checks_at_path_with_the_value(String,String)"
+});
+formatter.result({
+  "duration": 35297819,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 34,
+  "name": "Test my Get Method58",
+  "description": "",
+  "id": "first-rest-assured-test44;test-my-get-method58",
+  "type": "scenario",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "line": 33,
+      "name": "@Regression"
+    }
+  ]
+});
+formatter.step({
+  "line": 35,
+  "name": "user start the rest asured test",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 36,
+  "name": "user hit the get request",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 37,
+  "name": "user checks 200 the status code",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 38,
+  "name": "user checks the count or size",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 39,
+  "name": "user checks \"albert_park\" value",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 40,
+  "name": "user checks at path \"MRData.CircuitTable.Circuits.circuitId[0]\" with the \"albert_park\" value",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "MyGetWithTagsSD.user_start_the_rest_asured_test()"
+});
+formatter.result({
+  "duration": 92728,
+  "status": "passed"
+});
+formatter.match({
+  "location": "MyGetWithTagsSD.user_hit_the_get_request()"
+});
+formatter.result({
+  "duration": 1178785696,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "200",
+      "offset": 12
+    }
+  ],
+  "location": "MyGetWithTagsSD.user_checks_the_status_code(int)"
+});
+formatter.result({
+  "duration": 217884,
+  "status": "passed"
+});
+formatter.match({
+  "location": "MyGetWithTagsSD.user_checks_the_count_or_size()"
+});
+formatter.result({
+  "duration": 221223633,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "albert_park",
+      "offset": 13
+    }
+  ],
+  "location": "MyGetWithTagsSD.user_checks_circuitId_value(String)"
+});
+formatter.result({
+  "duration": 31545431,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "MRData.CircuitTable.Circuits.circuitId[0]",
+      "offset": 21
+    },
+    {
+      "val": "albert_park",
+      "offset": 74
+    }
+  ],
+  "location": "MyGetWithTagsSD.user_checks_at_path_with_the_value(String,String)"
+});
+formatter.result({
+  "duration": 33242994,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 44,
+  "name": "Test my Get Method59",
+  "description": "",
+  "id": "first-rest-assured-test44;test-my-get-method59",
+  "type": "scenario",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "line": 43,
+      "name": "@Smoke"
+    }
+  ]
+});
+formatter.step({
+  "line": 45,
+  "name": "user start the rest asured test",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 46,
+  "name": "user hit the get request",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 47,
+  "name": "user checks 200 the status code",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 48,
+  "name": "user checks the count or size",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 49,
+  "name": "user checks \"albert_park\" value",
+  "keyword": "Then "
+});
+formatter.step({
+  "line": 50,
+  "name": "user checks at path \"MRData.CircuitTable.Circuits.circuitId[0]\" with the \"albert_park\" value",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "MyGetWithTagsSD.user_start_the_rest_asured_test()"
+});
+formatter.result({
+  "duration": 92160,
+  "status": "passed"
+});
+formatter.match({
+  "location": "MyGetWithTagsSD.user_hit_the_get_request()"
+});
+formatter.result({
+  "duration": 1231605278,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "200",
+      "offset": 12
+    }
+  ],
+  "location": "MyGetWithTagsSD.user_checks_the_status_code(int)"
+});
+formatter.result({
+  "duration": 208782,
+  "status": "passed"
+});
+formatter.match({
+  "location": "MyGetWithTagsSD.user_checks_the_count_or_size()"
+});
+formatter.result({
+  "duration": 194141701,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "albert_park",
+      "offset": 13
+    }
+  ],
+  "location": "MyGetWithTagsSD.user_checks_circuitId_value(String)"
+});
+formatter.result({
+  "duration": 56040059,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "MRData.CircuitTable.Circuits.circuitId[0]",
+      "offset": 21
+    },
+    {
+      "val": "albert_park",
+      "offset": 74
+    }
+  ],
+  "location": "MyGetWithTagsSD.user_checks_at_path_with_the_value(String,String)"
+});
+formatter.result({
+  "duration": 37095506,
   "status": "passed"
 });
 });
